7db7f3d2456910fa219d814fedebc7254831434d
Bab <baptiste7792000@gmail.com>
2022-03-14 15:50:15 +0100

    modification one

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,34 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
+
+
+
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+console.log(modalbg);
 const modalBtn = document.querySelectorAll(".modal-btn");
+console.log(modalBtn);
 const formData = document.querySelectorAll(".formData");
+console.log(formData);
+
+
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => {
+    console.log(btn);
+    btn.addEventListener("click", launchModal)
+});
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
 
